8cd6754b8aaa685887a81beb07c23855bd9f9da1
Nick <nickceglia@gmail.com>
2019-05-01 16:11:05 -0400

    typo

--- src/pseudoaligner.rs
+++ src/pseudoaligner.rs
@@ -315,7 +315,7 @@ pub fn process_reads<K: Kmer + Sync + Send, P: AsRef<Path> + Debug>(
     reader: fastq::Reader<File>,
     index: &Pseudoaligner<K>,
     output: P,
-) -> Result<Vec::<(bool, std::string::String, std::vec::Vec<u32>, usize)>:, Error> {
+) -> Result<Vec::<(bool, std::string::String, std::vec::Vec<u32>, usize)>, Error> {
     info!("Done Reading index");
     info!("Starting Multi-threaded Mapping");
     // info!("Output filename: {:?}", output);
